3a399fb0878524220132156c440cb950949c2421
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-04-08 16:35:30 +0300

    test: Обновил тесты для Emitter-а

--- test/utils/Emitter/EmitterSimple.test.js
+++ test/utils/Emitter/EmitterSimple.test.js
@@ -1,7 +1,9 @@
 const { describe, it } = require('mocha');
 const { expect } = require('chai');
 
-const emitter = require('./../../src/utils/emitter');
+const EmitterSimple = require('./../../src/utils/Emitter/EmitterSimple');
+
+const emitter = new EmitterSimple();
 
 describe('emitter', () => {
   it('вызов обработчика при возникновении события', () => {
