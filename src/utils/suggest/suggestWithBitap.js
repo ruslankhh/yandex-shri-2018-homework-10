2d1c626cd7bad7711a04bdcedcab98b652e67664
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-04-11 00:30:44 +0300

    feat: Попытка реализации suggestWithBitap (тесты не прошли)

---
+++ src/utils/suggest/suggestWithBitap.js
@@ -0,0 +1,38 @@
+const preIncludes = (needle) => {
+  let lim = 0;
+  let S = [];
+
+  for (let i = 0; i < 256; i++) {
+    S[i] = ~0;
+  }
+
+  for (let i = 0, j = 1; i < needle.length; i++, j <<= 1) {
+    S[needle[i]] &= ~j;
+    lim |= j;
+  }
+
+  return ~(lim >> 1);
+};
+
+function includes (needle, haystack) {
+  let lim = preIncludes(needle);
+  let state = ~0;
+  let S = [];
+
+  for (let i = 0; i < haystack.length; i++) {
+    state = (state << 1) | S[haystack[i]];
+    if (state < lim) {
+      return i - needle.length !== -1;
+    }
+  }
+
+  return false;
+}
+
+function suggestWithBitap (input, collection) {
+  return collection.filter(item => {
+    includes(input.toLowerCase(), item.toLowerCase());
+  });
+}
+
+export default suggestWithBitap;
